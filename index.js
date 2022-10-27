b2a74813578245d0de09bc1f6ed52a37778b791f
Mohamed Ali Belkhir <medalibel@LAPTOP-PAVILION14.localdomain>
2022-10-27 11:55:14 +0100

    added doctor tables:clinic_info, working times

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const doctor_info = require('./doctor_info');
 
 const app = express();
 app.set('view engine','ejs');
 
-app.use(express.static("public"))
+app.use(express.static("public"));
+
+
+var doctor_insert_sql = "insert into doctor(email,password,first_name,last_name,specialty) values(doctor@gmail.com,doctor,med,belkhir,cardiology);"
+var clinic_insert_sql = "insert into clinic_info(doctor_id,wilaya,address,contact_num,is_working) values();"
+
 
 app.get('/',(req,res)=>{
     res.render('index');
 });
+app.get('/signup',(req,res)=>{
+    //res.render('accounttype')
+});
+app.get('/login',(req,res)=>{
+    //res.render('accounttype')
+});
 
+app.post('/signup/doctor',(req,res)=>{
+
+});
+
+app.get('/signup/doctor',(req,res)=>{
+    //res.render('doctorsignup')
+});
