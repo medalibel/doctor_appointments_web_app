b17fdbf5f58db488a8f5f745b8ced9194119cb3b
Mohamed Ali Belkhir <medalibel@LAPTOP-PAVILION14.localdomain>
2022-11-02 22:12:55 +0100

    added sign up functionality without sessions

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const doctor_info = require('./doctor_info');
 const dao = require('./dao');
-
-dao.getAllDoctors();
-dao.close_connection();
 
 const app = express();
 app.set('view engine','ejs');
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
-var doctor_insert_sql = "insert into doctor(email,password,first_name,last_name,specialty) values(doctor@gmail.com,doctor,med,belkhir,cardiology);"
 var clinic_insert_sql = "insert into clinic_info(doctor_id,wilaya,address,contact_num,is_working) values();"
 
 
@@ -22,13 +21,43 @@ app.get('/signup',(req,res)=>{
     //res.render('accounttype')
 });
 app.get('/login',(req,res)=>{
-    //res.render('accounttype')
-});
-
-app.post('/signup/doctor',(req,res)=>{
-
+    res.render('accountselection')
 });
 
 app.get('/signup/doctor',(req,res)=>{
-    //res.render('doctorsignup')
+    res.render('signup_test');
+});
+app.post('/signup/doctor',(req,res)=>{
+    let doctor={
+        first_name:req.body.first_name,
+        last_name:req.body.last_name,
+        email:req.body.email,
+        password:req.body.password,
+        specialty:req.body.specialty
+    }
+    dao.doctorSignUp(doctor,(doctor_id)=>{
+        console.log(doctor_id);
+        res.render('signup_test');
+    });
+});
+
+app.get('/login/doctor',(req,res)=>{
+    res.render('login_test');
+});
+app.post('/login/doctor',(req,res)=>{
+    if(!req.body.email || !req.body.password){
+        res.status(400).render('login_test');
+    }
+    else{
+        dao.doctorLogIn(req.body.email,(rows)=>{
+            console.log(rows);
+            res.status(200).render('login_test');
+        });
+        
+    }
+});
+
+
+app.listen(3000,()=>{
+    console.log('server listening on port 3000');
 });
